39b5ebb2fa1fdc5203d4bd27b7918b4861dd46a3
amolthakare <100188653+amolthakare@users.noreply.github.com>
2022-09-07 15:32:04 +0530

    string

---
+++ Javascript-IX Strings (IW)/problem1.js
@@ -0,0 +1,9 @@
+var str = "masai School A transformation in education";
+var count = 1;
+
+for (var i = 0; i < str.length; i++) {
+  if (str[i] == " ") {
+    count++;
+  }
+}
+console.log(count);
---
+++ Javascript-IX Strings (IW)/problem2.js
@@ -0,0 +1,17 @@
+// var str = "masai School";
+// var count=0;
+
+// for(var i=0;i<str.length;i++){
+//   if(str[i]!=" "){
+//     count++;
+//   }
+// }
+// console.log(count);
+
+var arr = ["virat", "rohit", "rahul"];
+var total_char = 0;
+
+for (var i = 0; i < arr.length; i++) {
+  total_char += arr[i].length;
+}
+console.log(total_char);
